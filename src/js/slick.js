e4a38ec550e12bcb56d2bbddd8a80bad272147cd
David Berka <davidberka2005@gmail.com>
2022-09-17 17:47:31 +0200

    Fixes

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,34 +1,43 @@
 $(document).ready(function () {
-    $('.topstory-carousel').slick({
-        centerMode: true,
-        slidesToShow: 1,
-        arrows: false,
-        variableWidth: true,
-        autoplay: true,
-        autoplaySpeed: 6000,
-        infinite: true,
-        arrows: true,
-        prevArrow: $('.slick-prev'),
-        nextArrow: $('.slick-next'),
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    dots: true,
-                    arrows: false,
+    $('.topstory-carousel')
+        .slick({
+            centerMode: true,
+            slidesToShow: 1,
+            arrows: false,
+            variableWidth: true,
+            autoplay: true,
+            autoplaySpeed: 6000,
+            infinite: true,
+            arrows: true,
+            prevArrow: $('.slick-prev'),
+            nextArrow: $('.slick-next'),
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        dots: true,
+                        arrows: false,
+                    },
                 },
-            },
-            {
-                breakpoint: 580,
-                settings: {
-                    centerMode: false,
-                    variableWidth: false,
-                    dots: true,
-                    arrows: false,
+                {
+                    breakpoint: 580,
+                    settings: {
+                        centerMode: false,
+                        variableWidth: false,
+                        dots: true,
+                        arrows: false,
+                    },
                 },
-            },
-        ],
-    });
+            ],
+        })
+        .on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+            if (currentSlide !== nextSlide) {
+                document.querySelectorAll('.slick-center + .slick-cloned').forEach((next) => {
+                    // timeout required or Slick will overwrite the classes
+                    setTimeout(() => next.classList.add('slick-current', 'slick-center'));
+                });
+            }
+        });
 
     $('.social-media-carousel').slick({
         infinite: true,
